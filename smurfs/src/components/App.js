8bd269366e0f8dbd5f204d424b53eb33adb92a6d
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-08 11:32:35 -0500

    Wired up App to handle pass down Redux state to Smurfs component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import { connect } from 'react-redux';
+import { getSmurfs, addSmurf } from '../actions';
+
+import Smurfs from './Smurfs';
+
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
+
 class App extends Component {
+
+  componentDidMount() {
+    this.props.getSmurfs()
+  }
+
   render() {
+    if (this.props.fetchingSmurfs) {
+      return <h1>LOADING</h1>;
+    }
+    
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <div>Welcome to the smurf village!</div>
+        <Smurfs smurfs={this.props.smurfs} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  }
+}
+
+export default connect(mapStateToProps, { getSmurfs, addSmurf })(App);
